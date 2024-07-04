75a37ff1cdd6d35edde1fe31d9a3a6299ed36b7d
ANDREY SHVYRKUNOV <Andreykashv03@mail.ru>
2024-07-04 13:06:12 +0400

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+const cardsSwiper = new Swiper(".js-swiper", {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".js-arrow-next",
+        prevEl: ".js-arrow-prev",
+    },
+});
